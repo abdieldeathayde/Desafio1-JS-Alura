1b3b208270308511ff8f2b34dc9d1d8e5acf78a8
Athay <abdieldeathayde67@gmail.com>
2025-03-05 21:11:00 -0300

    atualizando adicionar lista usuarios

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
-import Math
-let lista_sorteio = [];
+
 
 
 
 function adicionarAmigo() {
+    
 
-   
+    sorteio = null;
+    let lista_sorteio = [];
 
     let nome_amigo_sorteio = document.getElementById("amigo").value;
     
@@ -19,21 +20,28 @@ function adicionarAmigo() {
 
     listaAmigos.innerHTML += `<li><p>Amigo adicionado: ${nome_amigo_sorteio} </p></li>`;
 
+    
+
 }
 
-function sortearAmigo() {
+function sortearAmigo(){
 
-    Math.random(0, lista_sorteio);
+    let listaAmigos = document.getElementById("resultado");
 
-    
-
-
-    for (let contador = 0; contador < lista_sorteio; contador++) {
-
+    if (this.usuario < 2) {
+        alert("Adicione pelo menos 2 amigos para sortear!");
+        return;
     }
 
+    const indice = Math.floor(Math.random() * listaAmigos.length);
     
+    listaAmigos.innerHTML += `<li><p>Amigo Secreto: ${listaAmigos.indice}! </p></li>`;
 
+
+    //listaAmigos[indice]
+    // lista_sorteio.push(contador, listaAmigos[contador]);
+
+    // document.querySelector(".result").textContent = `Seu amigo secreto é: ${listaAmigos[indice]}`;
 
     
 }
