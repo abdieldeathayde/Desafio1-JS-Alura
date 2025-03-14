2a9b6672a60fe3b8077c6df87f88a488380764ce
Athay <abdieldeathayde67@gmail.com>
2025-03-14 19:57:14 -0300

    finalizando efeitos site amigo secreto

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ function adicionarAmigo() {
     listaAmigosElement.innerHTML += `<li><p>Amigo adicionado: ${nome_amigo_sorteio} </p></li>`;
 }
 
+
 function sortearAmigo() {
     if (listaAmigos.length < 2) {
         alert("Adicione pelo menos 2 amigos para sortear!");
@@ -25,4 +26,21 @@ function sortearAmigo() {
 
     let listaAmigosElement = document.getElementById("resultado");
     listaAmigosElement.innerHTML += `<li><p>Amigo Secreto: ${amigoSorteado}! </p></li>`;
+
+    let params = {
+		particleCount: 500, // Quantidade de confetes
+		spread: 90, // O quanto eles se espalham
+		startVelocity: 70, // Velocidade inicial
+		origin: { x: 0, y: 0.5 }, // Posição inicial na tela
+		angle: 45 // Ângulo em que os confetes serão lançados
+	};
+
+	// Joga confetes da esquerda pra direita
+	confetti(params);
+
+	// Joga confetes da direita para a esquerda
+	params.origin.x = 1;
+	params.angle = 135;
+	confetti(params);
 }
+
